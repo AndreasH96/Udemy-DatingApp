86614131248052d47e13cb09bc6b5be21c75749d
Andreas <andreashaggstrom96@gmail.com>
2019-06-27 22:01:34 +0200

    Started on Safety

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -3,10 +3,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Data
 {
-    public class DataContext : DbContext    //Inheriting from DbContext
+    public class DataContext : DbContext    //Inheriting from DbContext, a session with the application Database
     {
         public DataContext(DbContextOptions<DataContext> options) : base (options){ }
 
         public DbSet<Value> Values{get;set;}
+        public DbSet<User> Users { get; set; }
     }
 }
